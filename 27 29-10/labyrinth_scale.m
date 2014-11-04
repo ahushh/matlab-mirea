b8ed26aac5cb0701dc8db326cd9355905c003d8f
Ahushh <ahushh@gmail.com>
2014-11-04 06:46:51 +0300

    Solve 27th problem

--- 27 29-10/labyrinth_scale.m
+++ 27 29-10/labyrinth_scale.m
@@ -1,38 +1,45 @@
 function labyrinth_scale(r)
     maxx = 0; maxy = 0;
     x = 0; y = 0;
-    function do(side)
-        if ~r.is_mark()
-            r.mark;
-            if ~r.is_bord('n');
-                r.step('n'); do('n'); r.step('s');
-            end
-            if ~r.is_bord('s');
-                r.step('s'); do('s'); r.step('n');
-            end
-            if ~r.is_bord('w');
-                r.step('w'); do('w'); r.step('o');
-            end
-            if ~r.is_bord('o');
-                r.step('o'); do('o'); r.step('w');
-            end
-        end
+    function do()
         if x > maxx
             maxx = x;
         elseif y > maxy
             maxy = y;
         end
-        if side == 'n'
-            x = x + 1;
-        elseif side == 'o'
-            y = y + 1;
-        elseif side == 'w'
-            y = y - 1;
-        elseif side == 's'
-            x = x - 1;
+        if ~r.is_mark()
+            r.mark;
+            if ~r.is_bord('n');
+                y = y + 1;
+                r.step('n');
+                do();
+                r.step('s');
+                y = y - 1;
+            end
+            if ~r.is_bord('s');
+                y = y - 1;
+                r.step('s');
+                do();
+                r.step('n');
+                y = y + 1;
+            end
+            if ~r.is_bord('w');
+                x = x - 1;
+                r.step('w');
+                do();
+                r.step('o');
+                x = x + 1;
+            end
+            if ~r.is_bord('o');
+                x = x + 1;
+                r.step('o');
+                do();
+                r.step('w');
+                x = x - 1;
+            end
         end
     end
-    do('nope');
-    display(maxx);
-    display(maxy);
+    do();
+    display(maxx+1);
+    display(maxy+1);
 end
